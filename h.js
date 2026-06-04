4f6825f5c8c20fb60d3f682c4b41276475636552
Ahmed1 <uuuuoooiii777@gmail.com>
2026-06-05 00:54:44 +0300

    Add files via upload

---
+++ h.js
@@ -0,0 +1,15 @@
+const cars = [
+  {
+    firtcar:"BMW",
+    version: 2020,
+    
+  }, 
+  {
+    secondcar: "MarcedsBenz", 
+    version: 2021,
+  }
+];
+
+cars.forEach((car,i)=>{
+  console.log(car.firtcar,car.secondcar,car.version)
+}) 
